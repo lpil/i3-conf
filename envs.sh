5813e2e89a3154a4a5cfe5ca4ebdc968b3cc1d5e
Louis Pilfold <louis@lpil.uk>
2015-09-29 14:59:49 +0100

    Make env.sh saver

--- envs.sh
+++ envs.sh
@@ -1,13 +1,21 @@
-#!/bin/sh
+#!/usr/bin/env bash
+
+set -e
 
 # Ruby
-git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
-git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+if ! [ -d "$HOME/.rbenv" ]; then
+  git clone https://github.com/sstephenson/rbenv.git ~/.rbenv
+  git clone https://github.com/sstephenson/ruby-build.git ~/.rbenv/plugins/ruby-build
+fi
 
 # Node
-git clone https://github.com/OiNutter/nodenv.git ~/.nodenv
-git clone https://github.com/OiNutter/node-build.git ~/.nodenv/plugins/node-build
+if ! [ -d "$HOME/.nodenv" ]; then
+  git clone https://github.com/OiNutter/nodenv.git ~/.nodenv
+  git clone https://github.com/OiNutter/node-build.git ~/.nodenv/plugins/node-build
+fi
 
 # Elixir
-git clone git://github.com/mururu/exenv.git ~/.exenv
-git clone git://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
+if ! [ -d "$HOME/.exenv" ]; then
+  git clone git://github.com/mururu/exenv.git ~/.exenv
+  git clone git://github.com/mururu/elixir-build.git ~/.exenv/plugins/elixir-build
+fi
